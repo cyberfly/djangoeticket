8a7733860618e3e47f649d4b4b33836d94cfd49f
Fathur Rahman <cyberflyx@gmail.com>
2025-06-17 16:27:33 +0800

    continue create form, store department and category

--- tickets/views.py
+++ tickets/views.py
@@ -1,30 +1,51 @@
 from django.shortcuts import render
 from .models import Ticket
+from lookups.models import Department, Category
+
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    return render(request, "home.html")
+
 
 def create(request):
 
-    if request.method == 'POST':
+    if request.method == "POST":
         # bila submit form
 
         #  print untuk lihat data yang dihantar
         print("Form data", request.POST)
 
         # get data from post request
-        title = request.POST.get('title', '')
-        description = request.POST.get('description', '')
+        title = request.POST.get("title", "")
+        description = request.POST.get("description", "")
+        category_id = request.POST.get("category_id", "")
+        department_id = request.POST.get("department_id", "")
 
         user = request.user
+        department = Department.objects.get(id=department_id)
+        category = Category.objects.get(id=category_id)
 
         # try create a new ticket
-        Ticket.objects.create(title=title, description=description, user=user)
-        
-        
+        Ticket.objects.create(
+            title=title,
+            description=description,
+            user=user,
+            department=department,
+            category=category,
+        )
+
     else:
         # bila papar form
         pass
 
-    return render(request, 'tickets/create.html')
+    # load additional data for the form
+    departments = Department.objects.all()
+    categories = Category.objects.all()
+
+    context = {
+        "departments": departments,
+        "categories": categories,
+    }
+
+    return render(request, "tickets/create.html", context)
