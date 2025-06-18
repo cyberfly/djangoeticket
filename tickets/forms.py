ec19bb4e288759bf578a25e5947eaf87a676ef4a
Fathur Rahman <cyberflyx@gmail.com>
2025-06-18 10:46:35 +0800

    hold category_id and department_id when validation error

--- tickets/forms.py
+++ tickets/forms.py
@@ -3,3 +3,5 @@ from django import forms
 class CreateTicketForm(forms.Form):
     title = forms.CharField(max_length=200, required=True, label="Title", min_length=5)
     description = forms.CharField(required=True, label="Description", min_length=5)
+    category_id = forms.IntegerField(required=True, label="Category")
+    department_id = forms.IntegerField(required=True, label="Department")
