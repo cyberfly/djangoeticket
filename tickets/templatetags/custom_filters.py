371dba070dbf75c5e52a7ac348eda3adaf196057
Fathur Rahman <cyberflyx@gmail.com>
2025-06-19 10:24:36 +0800

    implement filter format_ticket_status to style status badge

--- tickets/templatetags/custom_filters.py
+++ tickets/templatetags/custom_filters.py
@@ -1,5 +1,5 @@
 from django import template
-
+from django.utils.safestring import mark_safe
 register = template.Library()
 
 @register.simple_tag(takes_context=True)
@@ -8,3 +8,16 @@ def query_transform(context, **kwargs):
     for k, v in kwargs.items():
         query[k] = v
     return query.urlencode()
+
+@register.filter(name='format_ticket_status')
+def format_ticket_status(status):
+    if status == 'open':
+        return mark_safe('<span class="badge bg-warning">'+status+'</span>')
+    
+    if status == 'in_progress':
+        return mark_safe('<span class="badge bg-info">'+status+'</span>')
+    
+    if status == 'closed':
+        return mark_safe('<span class="badge bg-success">'+status+'</span>')
+
+    return status
